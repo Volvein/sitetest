aaa79bcab538fb9ad49cf2ef264dd1ec532e11b9
Lokendra Sharma <84660404+Volvein@users.noreply.github.com>
2022-12-27 14:29:48 +0530

    initalize

---
+++ Js/editablepage.js
@@ -0,0 +1,16 @@
+﻿var someSessionVariable = @Session["Id"];
+if (someSessionVariable > 0) {
+    $(document).ready(() => {
+        const box = document.querySelectorAll("#contentR");
+        const box1 = document.querySelectorAll(".contentR");
+        box.forEach(x => {
+            $(x).attr({ 'contenteditable': 'true' });
+            $(".edit-icon").show();
+        })
+        box1.forEach(x => {
+            $(x).attr({ 'contenteditable': 'true' });
+        })
+    }
+    )
+
+}
---
+++ Js/test3.js
@@ -0,0 +1,10 @@
+﻿var link = document.querySelectorAll('.onclickbtn');
+link.forEach((arrayElement, index) => {
+    link[index].removeAttribute('onclick');
+});
+
+
+let menuToggle = document.querySelector(".toggle");
+menuToggle.onclick = function() {
+  menuToggle.classList.toggle("active")
+}
